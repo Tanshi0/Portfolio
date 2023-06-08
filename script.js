6c2a291d91181ae29f695d159b070e88a37d9dfa
Bogatyreva Natalya <124141494+Tanshi0@users.noreply.github.com>
2023-06-08 10:32:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+/*скрипт анимации при нажатии на ссылку*/
+$('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
+
+/*скрипт анимации при прокрутке контента */
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('content-show', );
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.content');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
